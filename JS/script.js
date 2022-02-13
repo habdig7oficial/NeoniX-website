63b4e8ca4b22899c965addc9c26a081e70537461
habdig7oficial <65318468+habdig7oficial@users.noreply.github.com>
2022-02-13 19:48:46 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,22 @@
+      /*Aviso de Licença do Arquivo */
+
+      /* Licença Disponível no arquivo COPYING*/
+
+      /*-Copyright © 2022 Mateus Felipe da Silveira Vieira*/
+
+      /*Esta Página (NeoniX Page) é um software livre; você pode redistribuí-lo e/ou
+      modificá-lo sob os termos da Licença Pública Geral GNU como publicada
+      pela Free Software Foundation; na versão 3 da Licença, ou qualquer versão posterior.
+    .
+      Este programa é distribuído na esperança de que possa ser útil,
+      mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+      a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+      Licença Pública Geral GNU para mais detalhes.
+      Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+      com este programa. Se não, veja <http://www.gnu.org/licenses/>.*/
+
+      /*NOTA: O Sistema Operacional NeoniX não compartilha a Lincença GNU GPL 3. Cada componte possuí suas proóprias liçencas */
+
 document.addEventListener("DOMContentLoaded",function () {
 
     iso = document.querySelector("#iso")
